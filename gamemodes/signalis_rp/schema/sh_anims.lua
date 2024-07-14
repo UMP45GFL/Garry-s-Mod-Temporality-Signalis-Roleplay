738e245efa9ea53f22ac68c870e1451f9cf95c73
Maja552 <mikazukikanade@gmail.com>
2024-07-14 13:25:53 +0200

    Removed helix addon, moved animations from there to here

---
+++ gamemodes/signalis_rp/schema/sh_anims.lua
@@ -0,0 +1,145 @@
+
+
+-- REPLIKA ANIMATIONS
+ix.anim.replika = {
+	normal = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE, ACT_IDLE_ANGRY},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_COVER_LOW},
+		
+		[ACT_MP_WALK] = {"walkunarmed_all", "walkunarmed_all"},
+		
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH, ACT_WALK_CROUCH_AIM_RIFLE},
+		--[ACT_MP_RUN] = {ACT_RUN, ACT_RUN_AIM_RIFLE_STIMULATED},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET}
+	},
+	/*
+	pistol = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE_PISTOL, ACT_IDLE_ANGRY_PISTOL},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_RANGE_AIM_SMG1_LOW},
+		[ACT_MP_WALK] = {ACT_WALK, ACT_WALK_AIM_PISTOL},
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH, ACT_WALK_CROUCH_AIM_RIFLE},
+		[ACT_MP_RUN] = {ACT_RUN, ACT_RUN_AIM_PISTOL},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET},
+		attack = ACT_GESTURE_RANGE_ATTACK_PISTOL,
+		reload = ACT_RELOAD_PISTOL
+	},
+	*/
+	smg = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE_SMG1_RELAXED, ACT_IDLE_ANGRY_SMG1},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_RANGE_AIM_SMG1_LOW},
+		[ACT_MP_WALK] = {ACT_WALK_RIFLE_RELAXED, ACT_WALK_AIM_RIFLE_STIMULATED},
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH_RIFLE, ACT_WALK_CROUCH_AIM_RIFLE},
+		[ACT_MP_RUN] = {ACT_RUN_RIFLE_RELAXED, ACT_RUN_AIM_RIFLE_STIMULATED},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET},
+		attack = ACT_GESTURE_RANGE_ATTACK_SMG1,
+		reload = ACT_GESTURE_RELOAD_SMG1
+	},
+	shotgun = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE_SHOTGUN_RELAXED, ACT_IDLE_ANGRY_SMG1},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_RANGE_AIM_SMG1_LOW},
+		[ACT_MP_WALK] = {ACT_WALK_RIFLE_RELAXED, ACT_WALK_AIM_RIFLE_STIMULATED},
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH_RIFLE, ACT_WALK_CROUCH_AIM_RIFLE},
+		[ACT_MP_RUN] = {ACT_RUN_RIFLE_RELAXED, ACT_RUN_AIM_RIFLE_STIMULATED},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET},
+		attack = ACT_GESTURE_RANGE_ATTACK_SHOTGUN
+	},
+	grenade = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE, ACT_IDLE_MANNEDGUN},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_RANGE_AIM_SMG1_LOW},
+		[ACT_MP_WALK] = {ACT_WALK, ACT_WALK_AIM_PISTOL},
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH, ACT_WALK_CROUCH_AIM_RIFLE},
+		[ACT_MP_RUN] = {ACT_RUN, ACT_RUN_AIM_PISTOL},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET},
+		attack = ACT_RANGE_ATTACK_THROW
+	},
+	melee = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE, ACT_IDLE_MANNEDGUN},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_COVER_LOW},
+		[ACT_MP_WALK] = {ACT_WALK, ACT_WALK_AIM_RIFLE},
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH, ACT_WALK_CROUCH},
+		[ACT_MP_RUN] = {ACT_RUN, ACT_RUN},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET},
+		attack = ACT_MELEE_ATTACK_SWING
+	},
+	glide = ACT_GLIDE,
+	vehicle = ix.anim.citizen_male.vehicle
+}
+
+ix.anim.gestalt = {
+	normal = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE, ACT_IDLE_ANGRY},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_COVER_LOW},
+		
+		[ACT_MP_WALK] = {"walkunarmed_all", "walkunarmed_all"},
+		
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH, ACT_WALK_CROUCH_AIM_RIFLE},
+		--[ACT_MP_RUN] = {ACT_RUN, ACT_RUN_AIM_RIFLE_STIMULATED},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET}
+	},
+	/*
+	pistol = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE_PISTOL, ACT_IDLE_ANGRY_PISTOL},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_RANGE_AIM_SMG1_LOW},
+		[ACT_MP_WALK] = {ACT_WALK, ACT_WALK_AIM_PISTOL},
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH, ACT_WALK_CROUCH_AIM_RIFLE},
+		[ACT_MP_RUN] = {ACT_RUN, ACT_RUN_AIM_PISTOL},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET},
+		attack = ACT_GESTURE_RANGE_ATTACK_PISTOL,
+		reload = ACT_RELOAD_PISTOL
+	},
+	*/
+	smg = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE_SMG1_RELAXED, ACT_IDLE_ANGRY_SMG1},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_RANGE_AIM_SMG1_LOW},
+		[ACT_MP_WALK] = {ACT_WALK_RIFLE_RELAXED, ACT_WALK_AIM_RIFLE_STIMULATED},
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH_RIFLE, ACT_WALK_CROUCH_AIM_RIFLE},
+		[ACT_MP_RUN] = {ACT_RUN_RIFLE_RELAXED, ACT_RUN_AIM_RIFLE_STIMULATED},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET},
+		attack = ACT_GESTURE_RANGE_ATTACK_SMG1,
+		reload = ACT_GESTURE_RELOAD_SMG1
+	},
+	shotgun = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE_SHOTGUN_RELAXED, ACT_IDLE_ANGRY_SMG1},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_RANGE_AIM_SMG1_LOW},
+		[ACT_MP_WALK] = {ACT_WALK_RIFLE_RELAXED, ACT_WALK_AIM_RIFLE_STIMULATED},
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH_RIFLE, ACT_WALK_CROUCH_AIM_RIFLE},
+		[ACT_MP_RUN] = {ACT_RUN_RIFLE_RELAXED, ACT_RUN_AIM_RIFLE_STIMULATED},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET},
+		attack = ACT_GESTURE_RANGE_ATTACK_SHOTGUN
+	},
+	grenade = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE, ACT_IDLE_MANNEDGUN},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_RANGE_AIM_SMG1_LOW},
+		[ACT_MP_WALK] = {ACT_WALK, ACT_WALK_AIM_PISTOL},
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH, ACT_WALK_CROUCH_AIM_RIFLE},
+		[ACT_MP_RUN] = {ACT_RUN, ACT_RUN_AIM_PISTOL},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET},
+		attack = ACT_RANGE_ATTACK_THROW
+	},
+	melee = {
+		[ACT_MP_STAND_IDLE] = {ACT_IDLE, ACT_IDLE_MANNEDGUN},
+		[ACT_MP_CROUCH_IDLE] = {ACT_COVER_LOW, ACT_COVER_LOW},
+		[ACT_MP_WALK] = {ACT_WALK, ACT_WALK_AIM_RIFLE},
+		[ACT_MP_CROUCHWALK] = {ACT_WALK_CROUCH, ACT_WALK_CROUCH},
+		[ACT_MP_RUN] = {ACT_RUN, ACT_RUN},
+		[ACT_LAND] = {ACT_RESET, ACT_RESET},
+		attack = ACT_MELEE_ATTACK_SWING
+	},
+	glide = ACT_GLIDE,
+	vehicle = ix.anim.citizen_male.vehicle
+}
+
+ix.anim.SetModelClass("models/citric/signalis_adlr/adler_pm.mdl", "replika")
+ix.anim.SetModelClass("models/citric/signalis_stcr/stcr_pm.mdl", "replika")
+ix.anim.SetModelClass("models/voxaid/signalis_star/star_pm.mdl", "replika")
+ix.anim.SetModelClass("models/voxaid/signalis_eule/eule_pm.mdl", "replika")
+ix.anim.SetModelClass("models/voxaid/araV2/araV2_pm.mdl", "replika")
+ix.anim.SetModelClass("models/voxaid/signalis_arar/arar_pm.mdl", "replika")
+ix.anim.SetModelClass("models/citric/signalis_fklr/falke_pm.mdl", "replika")
+ix.anim.SetModelClass("models/voxaid/signalis_kolibri/kolibri_pm.mdl", "replika")
+ix.anim.SetModelClass("models/citric/signalis_lstr/elster_pm.mdl", "replika")
+ix.anim.SetModelClass("models/voxaid/signalis_mynah/mynah_pm.mdl", "replika")
+
+ix.anim.SetModelClass("models/voxaid/alina/alina_pm.mdl", "gestalt")
+ix.anim.SetModelClass("models/citric/signalis_ariane/ariane_pm.mdl", "gestalt")
+ix.anim.SetModelClass("models/voxaid/isa/isa_pm.mdl", "gestalt")
