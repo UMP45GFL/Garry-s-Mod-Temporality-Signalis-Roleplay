3b168231742d83ac4ff26f30d442f38f55b9dae2
Maja552 <mikazukikanade@gmail.com>
2024-07-29 16:27:47 +0200

    Added a few more music tracks

--- gamemodes/signalis_rp/plugins/kanade_music.lua
+++ gamemodes/signalis_rp/plugins/kanade_music.lua
@@ -18,16 +18,18 @@ ix.option.Add("musicSystemEnabled", ix.type.bool, true, {
 
 local trackList = {
 	-- casual ones
-	{ name = "Turned Around", 	sound = "eternalis/signalis_ost/1_turned_around.mp3", volume = 1, length = 222 },
-	{ name = "Safe Room", 		sound = "eternalis/signalis_ost/2_safe_room.mp3", 	volume = 1, length = 138 },
-	{ name = "Double Back", 	sound = "eternalis/signalis_ost/4_double_back.mp3", 	volume = 1, length = 362 },
-	{ name = "Mnhr", 			sound = "eternalis/signalis_ost/12_mnhr.mp3", 		volume = 1, length = 294 },
-	{ name = "Dowsing", 		sound = "eternalis/signalis_ost/15_dowsing.mp3", 		volume = 1, length = 128 },
-	{ name = "Sea Smoke", 		sound = "eternalis/signalis_ost/32_sea_smoke.mp3", 	volume = 1, length = 134 },
-	{ name = "Crepuscular", 	sound = "eternalis/signalis_ost/54_crepuscular.mp3", 	volume = 1, length = 222 },
+	{ name = "Turned Around", 			sound = "eternalis/signalis_ost/1_turned_around.mp3", 			volume = 1, length = 222 },
+	{ name = "Safe Room", 				sound = "eternalis/signalis_ost/2_safe_room.mp3", 				volume = 1, length = 138 },
+	{ name = "Double Back", 			sound = "eternalis/signalis_ost/4_double_back.mp3", 			volume = 1, length = 362 },
+	{ name = "Double Back VHS ver.", 	sound = "eternalis/signalis_ost/48_double_back_vhs_ver.mp3", 	volume = 1, length = 362 },
+	{ name = "Mnhr", 					sound = "eternalis/signalis_ost/12_mnhr.mp3", 					volume = 1, length = 294 },
+	{ name = "Dowsing", 				sound = "eternalis/signalis_ost/15_dowsing.mp3", 				volume = 1, length = 128 },
+	{ name = "Crepuscular", 			sound = "eternalis/signalis_ost/54_crepuscular.mp3", 			volume = 1, length = 222 },
 
 	-- more sad or scary or intense
-	{ name = "Adler", 			sound = "signalis_ost/14_adler.mp3", 		volume = 1, length = 298 },
+	{ name = "Ritual", 					sound = "signalis_ost/11_ritual.mp3", 							volume = 1, length = 258 },
+	{ name = "Adler", 					sound = "signalis_ost/14_adler.mp3", 							volume = 1, length = 298 },
+	{ name = "Sea Smoke", 				sound = "eternalis/signalis_ost/32_sea_smoke.mp3", 				volume = 1, length = 134 },
 }
 
 if SERVER then
