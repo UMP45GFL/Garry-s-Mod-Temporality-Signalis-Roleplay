4518fbc87b9a5a4d7d85c1c821397773faaef0cf
Maja552 <mikazukikanade@gmail.com>
2024-07-31 13:57:59 +0200

    Made it so folders only accept papers

--- gamemodes/signalis_rp/schema/items/containers/sh_folder.lua
+++ gamemodes/signalis_rp/schema/items/containers/sh_folder.lua
@@ -3,3 +3,7 @@ ITEM.name = "Folder"
 ITEM.model = Model("models/eternalis/items/library/folder.mdl")
 ITEM.description = "A folder."
 ITEM.skin = 0
+
+ITEM.AcceptableItems = {
+    ["paper"] = true
+}
