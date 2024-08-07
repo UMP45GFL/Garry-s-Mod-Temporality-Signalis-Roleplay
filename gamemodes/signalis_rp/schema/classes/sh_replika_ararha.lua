50ffd9186c72d7eb84ddc17d1386737047f65100
Maja552 <mikazukikanade@gmail.com>
2024-08-07 20:49:57 +0200

    Added Heavy Armor ARAR

---
+++ gamemodes/signalis_rp/schema/classes/sh_replika_ararha.lua
@@ -0,0 +1,47 @@
+CLASS.name = "ARAR HA Replika"
+CLASS.faction = FACTION_REPLIKA
+CLASS.isDefault = false
+CLASS.availableByDefault = false
+CLASS.models = {
+	{
+		mdl = "models/voxaid/araV2/araV2_pm.mdl",
+		hullMins = Vector(-12, -12, 0),
+		hullMaxs = Vector(12, 12, 75)
+	},
+}
+CLASS.health = 120
+CLASS.armor = nil
+CLASS.physical_damage_taken = 0.7 -- titanium reinforced shell
+CLASS.bullet_damage_taken = 0.7 -- titanium reinforced shell
+CLASS.mental_strength = 0.92 -- arars are a bit more unstable...
+CLASS.hunger = 0.9
+CLASS.thirst = 0.9
+-- titanium shell makes them less mobile
+CLASS.speed = 0.85
+CLASS.jump_power = 0.9 -- not as bad because they are tall
+CLASS.max_stamina = 0.87
+
+-- attributes
+CLASS.remove_attributes = true
+
+CLASS.weapon_knowledge = 0
+CLASS.min_weapon_knowledge = 0
+CLASS.max_weapon_knowledge = 3
+CLASS.medical_knowledge = 0
+CLASS.min_medical_knowledge = 0
+CLASS.max_medical_knowledge = 3
+
+CLASS_REPLIKA_ARARHA = CLASS.index
+
+function CLASS:OnSet(client)
+end
+
+function CLASS:OnCharacterCreated(client, character)
+	local inventory = character:GetInventory()
+
+	inventory:Add("ration_k4", 1)
+	inventory:Add("id_arar", 1, {
+		skin = 3,
+		name = character:GetName()
+	})
+end
