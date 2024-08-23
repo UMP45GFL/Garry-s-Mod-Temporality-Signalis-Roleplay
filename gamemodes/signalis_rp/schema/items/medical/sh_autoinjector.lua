66919f14b16f65ff3d487af31d7c0542f9f3c059
Maja552 <mikazukikanade@gmail.com>
2024-08-23 21:07:45 +0200

    Made autoinjector equippable and functional

--- gamemodes/signalis_rp/schema/items/medical/sh_autoinjector.lua
+++ gamemodes/signalis_rp/schema/items/medical/sh_autoinjector.lua
@@ -7,3 +7,112 @@ ITEM.price = 60
 ITEM.addHealth = 120
 ITEM.applySound = "items/medshot4.wav"
 ITEM.replikaOnly = true
+
+-- Inventory drawing
+if (CLIENT) then
+	function ITEM:PaintOver(item, w, h)
+		if (item:GetData("equip")) then
+			surface.SetDrawColor(110, 255, 110, 100)
+			surface.DrawRect(w - 14, h - 14, 8, 8)
+		end
+	end
+end
+
+function ITEM:EquipAutoinjector(client)
+	self:SetData("equip", true)
+end
+
+function ITEM:UnequipAutoinjector(client)
+	self:SetData("equip", false)
+end
+
+ITEM:Hook("drop", function(item)
+	if (item:GetData("equip")) then
+		local character = ix.char.loaded[item.owner]
+		local client = character and character:GetPlayer() or item:GetOwner()
+
+		item.player = client
+        item:UnequipAutoinjector(client)
+	end
+end)
+
+ITEM.functions.EquipUn = {
+	name = "Unequip",
+	tip = "equipTip",
+	icon = "icon16/cross.png",
+	OnRun = function(item)
+		local character = ix.char.loaded[item.owner]
+		local client = character and character:GetPlayer() or item:GetOwner()
+
+        item:UnequipAutoinjector(client)
+		return false
+	end,
+	OnCanRun = function(item)
+		local client = item.player
+
+		return !IsValid(item.entity) and IsValid(client) and item:GetData("equip") == true and
+			hook.Run("CanPlayerUnequipItem", client, item) != false
+	end
+}
+
+ITEM.functions.Equip = {
+	name = "Equip",
+	tip = "equipTip",
+	icon = "icon16/tick.png",
+	OnRun = function(item)
+		local client = item.player
+		local char = client:GetCharacter()
+		local items = char:GetInventory():GetItems()
+
+		for _, v in pairs(items) do
+			if (v.uniqueID and item.uniqueID == "autoinjector" and v.id != item.id and v.data.equip) then
+				local itemTable = ix.item.instances[v.id]
+                return false
+			end
+		end
+
+		item:EquipAutoinjector(client)
+		return false
+	end,
+	OnCanRun = function(item)
+		local client = item.player
+
+		if client:Team() != FACTION_REPLIKA then
+			return false
+		end
+
+		return !IsValid(item.entity) and IsValid(client) and item:GetData("equip") != true and
+			hook.Run("CanPlayerEquipItem", client, item) != false
+	end
+}
+
+function ITEM:OnRemoved()
+	if (self.invID != 0 and self:GetData("equip")) then
+		self.player = self:GetOwner()
+        self:UnequipAutoinjector(self.player)
+		self.player = nil
+	end
+end
+
+hook.Add("EntityTakeDamage", "AutoinjectorPreventDeath", function(target, dmginfo)
+	if target:IsPlayer() and target:IsReplika() then
+        local items = target:GetCharacter():GetInventory():GetItems()
+
+        for _, v in pairs(items) do
+            if (v.uniqueID and v.uniqueID == "autoinjector" and v:GetData("equip")) then
+                local itemTable = ix.item.instances[v.id]
+
+                if (itemTable:GetData("equip")) then
+                    itemTable:UnequipAutoinjector(target)
+                end
+
+                target:SetHealth(math.min(target:Health() + itemTable.addHealth, target:GetMaxHealth()))
+                target:EmitSound(itemTable.applySound)
+
+                itemTable:Remove()
+
+                return true
+            end
+        end
+	end
+end)
