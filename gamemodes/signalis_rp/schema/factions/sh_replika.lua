843a9d0897b12f3698f74b3b37d16deb80d3a79e
Maja552 <mikazukikanade@gmail.com>
2024-08-28 17:04:26 +0200

    Made replikas globally recognized

--- gamemodes/signalis_rp/schema/factions/sh_replika.lua
+++ gamemodes/signalis_rp/schema/factions/sh_replika.lua
@@ -4,6 +4,7 @@ FACTION.description = "A biomechanical human made by AEON."
 FACTION.color = Color(194, 54, 8, 255)
 FACTION.isDefault = false
 FACTION.availableByDefault = false
+FACTION.isGloballyRecognized = true
 
 function FACTION:OnCharacterCreated(client, character)
 	local id = Schema:ZeroNumber(math.random(1, 99999), 5)
