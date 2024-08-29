6a91f0da24fe1827c9f0f91429fd0ed5dae14563
Maja552 <mikazukikanade@gmail.com>
2024-08-29 18:30:09 +0200

    Moved most signalis sounds from helix to schema

--- gamemodes/signalis_rp/schema/sh_player.lua
+++ gamemodes/signalis_rp/schema/sh_player.lua
@@ -23,32 +23,3 @@ end
 function meta:IsReplika()
 	return self:Team() == FACTION_REPLIKA
 end
-
-if SERVER then
-	util.AddNetworkString("sendErrorSound")
-	util.AddNetworkString("sendPlaySound")
-
-	function meta:SendErrorSound()
-		net.Start("sendErrorSound")
-		net.Send(self)
-	end
-
-	function meta:SendPlaySound(snd)
-		net.Start("sendPlaySound")
-		net.WriteString(snd)
-		net.Send(self)
-	end
-end
-
-if CLIENT then
-	net.Receive("sendErrorSound", function()
-		surface.PlaySound("eternalis/signalis_ui/no.wav")
-	end)
-
-	net.Receive("sendPlaySound", function()
-		local snd = net.ReadString()
-		if snd then
-			surface.PlaySound(snd)
-		end
-	end)
-end
---
+++ gamemodes/signalis_rp/schema/sh_sounds.lua
@@ -0,0 +1,60 @@
+﻿
+-- method to play a sound on the client
+if SERVER then
+    local meta = FindMetaTable("Player")
+
+	util.AddNetworkString("sendErrorSound")
+	util.AddNetworkString("sendPlaySound")
+
+	function meta:SendErrorSound()
+		net.Start("sendErrorSound")
+		net.Send(self)
+	end
+
+	function meta:SendPlaySound(snd)
+		net.Start("sendPlaySound")
+		net.WriteString(snd)
+		net.Send(self)
+	end
+end
+if CLIENT then
+	net.Receive("sendErrorSound", function()
+		surface.PlaySound("eternalis/signalis_ui/no.wav")
+	end)
+
+	net.Receive("sendPlaySound", function()
+		local snd = net.ReadString()
+		if snd then
+			surface.PlaySound(snd)
+		end
+	end)
+end
+
+-- Sounds
+if CLIENT then
+    -- char loaded
+    hook.Add("CharacterLoaded", "CharacterLoadedSound", function(char)
+        surface.PlaySound("eternalis/signalis_ui/character_selected2.wav")
+    end)
+
+    -- char load failed
+    hook.Add("CharacterLoadFailed", "CharacterLoadFailedSound", function(client, char)
+        surface.PlaySound("eternalis/signalis_ui/no.wav")
+    end)
+
+    -- char creation limit failure
+    hook.Add("OnCharacterLimitFailure", "OnCharacterLimitFailureSound", function(text)
+        surface.PlaySound("eternalis/signalis_ui/no.wav")
+    end)
+
+    -- keyboard sound
+    hook.Add("ChatTextChanged", "ChatTextChangedSound", function(text)
+        surface.PlaySound("eternalis/signalis_ui/keyboard_tap.wav")
+    end)
+else
+    -- character saved
+    hook.Add("OnCharacterCreated", "OnCharacterCreatedSound", function(client, char)
+        client:SendPlaySound("eternalis/signalis_ui/save.wav")
+    end)
+end
+
