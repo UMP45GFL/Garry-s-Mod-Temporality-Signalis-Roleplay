674c4a28db441b156a54d179a3540b742cb344cf
Maja552 <mikazukikanade@gmail.com>
2024-08-30 13:30:48 +0200

    Added iconCams to weapon items

--- gamemodes/signalis_rp/schema/items/weapons/sh_bw5.lua
+++ gamemodes/signalis_rp/schema/items/weapons/sh_bw5.lua
@@ -12,7 +12,7 @@ ITEM.width = 5
 ITEM.height = 1
 
 ITEM.iconCam = {
-	ang	= Angle(-17.581502914429, 250.7974395752, 0),
-	fov	= 5.412494001838,
-	pos	= Vector(57.109928131104, 181.7945098877, -60.738327026367)
+	pos = Vector(0, 200, -6.25),
+	ang = Angle(0, 270, 0),
+	fov = 20
 }
