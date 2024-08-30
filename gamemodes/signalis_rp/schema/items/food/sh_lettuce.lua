fc8fdf5f99c66c0fd7037f10de771d6d7c2b1232
Maja552 <mikazukikanade@gmail.com>
2024-08-30 19:02:07 +0200

    Changed the names of meat files and added lettuce

---
+++ gamemodes/signalis_rp/schema/items/food/sh_lettuce.lua
@@ -0,0 +1,12 @@
+
+ITEM.name = "Lettuce"
+ITEM.model = Model("models/foodnhouseholditems/lettuce.mdl")
+ITEM.width = 1
+ITEM.height = 1
+ITEM.description = "A lettuce. You don't know if it's fresh."
+
+ITEM.addHunger = -3
+ITEM.addThirst = -2
+ITEM.addHealth = 2
+ITEM.addStamina = 6
+ITEM.eatSound = nil
