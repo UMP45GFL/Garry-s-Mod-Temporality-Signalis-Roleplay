b674d94b660747f30cf224f4b32ea15bd036774f
Maja552 <mikazukikanade@gmail.com>
2024-08-30 18:44:10 +0200

    Made melon sliceable

---
+++ gamemodes/signalis_rp/schema/items/food/sh_melon.lua
@@ -0,0 +1,11 @@
+
+ITEM.name = "Watermelon"
+ITEM.model = Model("models/foodnhouseholditems/watermelon_unbreakable.mdl")
+ITEM.description = "A watermelon. It's juicy and sweet."
+
+ITEM.addHunger = -10
+ITEM.addThirst = -4
+ITEM.addHealth = 4
+ITEM.addStamina = 15
+ITEM.eatSound = nil
+ITEM.sliceableInto = {"melon_slice", 2}
--- gamemodes/signalis_rp/schema/items/food/sh_melon_slice.lua
+++ gamemodes/signalis_rp/schema/items/food/sh_melon_slice.lua
@@ -3,8 +3,8 @@ ITEM.name = "Watermelon slice"
 ITEM.model = Model("models/foodnhouseholditems/watermelon_slice.mdl")
 ITEM.description = "A slice of watermelon. It's juicy and sweet."
 
-ITEM.addHunger = -8
-ITEM.addThirst = -3
-ITEM.addHealth = 3
-ITEM.addStamina = 15
+ITEM.addHunger = -5
+ITEM.addThirst = -2
+ITEM.addHealth = 2
+ITEM.addStamina = 8
 ITEM.eatSound = nil
