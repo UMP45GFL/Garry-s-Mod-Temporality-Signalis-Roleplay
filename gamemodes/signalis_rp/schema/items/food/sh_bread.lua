a84b2ecfd8a9760de5145dd515eb79d97df64260
Maja552 <mikazukikanade@gmail.com>
2024-08-30 18:09:48 +0200

    Added bread slicing

--- gamemodes/signalis_rp/schema/items/food/sh_bread.lua
+++ gamemodes/signalis_rp/schema/items/food/sh_bread.lua
@@ -1,6 +1,6 @@
 
 ITEM.name = "Loaf of bread"
-ITEM.model = Model("models/foodnhouseholditems/bread-2.mdl")
+ITEM.model = Model("models/foodnhouseholditems/bread-1.mdl")
 ITEM.description = "A loaf of bread, it looks delicious."
 
 ITEM.addHunger = -20
@@ -8,3 +8,27 @@ ITEM.addThirst = 0
 ITEM.addHealth = 7
 ITEM.addStamina = 0
 ITEM.eatSound = nil
+
+function BreadSliceTemp(item)
+    local ply = item.player
+
+    if (IsValid(ply)) then
+        local inventory = ply:GetCharacter():GetInventory()
+        local breadSlice = inventory:Add("bread_slice")
+
+        if (breadSlice) then
+            inventory:Add("bread_slice")
+            inventory:Add("bread_slice")
+            inventory:Add("bread_slice")
+            return true
+        end
+    end
+
+    return false
+end
+
+ITEM.functions.Slice = {
+	OnRun = function(item)
+        return BreadSliceTemp(item)
+	end
+}
---
+++ gamemodes/signalis_rp/schema/items/food/sh_bread_slice.lua
@@ -0,0 +1,10 @@
+
+ITEM.name = "Slice of bread"
+ITEM.model = Model("models/foodnhouseholditems/bread_slice.mdl")
+ITEM.description = "A slice of bread, it looks delicious."
+
+ITEM.addHunger = -5
+ITEM.addThirst = 0
+ITEM.addHealth = 2
+ITEM.addStamina = 0
+ITEM.eatSound = nil
