688a773274a938f0ab2f152d09164d805f7b914e
Maja552 <mikazukikanade@gmail.com>
2024-08-30 18:56:28 +0200

    Made a tool base and put wrench socket and kitchen knife into it

---
+++ gamemodes/signalis_rp/schema/items/base/sh_tools.lua
@@ -0,0 +1,7 @@
+
+ITEM.name = "Tools Base"
+ITEM.model = "models/eternalis/items/misc/totem.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.category = "Tools"
+ITEM.description = "Base tools item"
---
+++ gamemodes/signalis_rp/schema/items/tools/sh_kitchen_knife.lua
@@ -0,0 +1,10 @@
+
+ITEM.name = "Kitchen knife"
+ITEM.model = Model("models/eternalis/items/weapons/kitchen/small_sharp_knife.mdl")
+ITEM.description = "A small, sharp kitchen knife. Perfect for cutting vegetables and meat."
+ITEM.skin = 0
+
+ITEM.width = 2
+ITEM.height = 1
+
+ITEM.canSlice = true
