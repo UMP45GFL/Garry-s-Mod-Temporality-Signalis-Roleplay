8b57acb08e85d8926de5836319672ea81407e0d8
Maja552 <mikazukikanade@gmail.com>
2024-09-01 17:31:55 +0200

    Made it so longer words get split in the signalis talk sounds plugin

--- gamemodes/signalis_rp/plugins/kanade_signalis_talk_sounds.lua
+++ gamemodes/signalis_rp/plugins/kanade_signalis_talk_sounds.lua
@@ -15,53 +15,67 @@ ix.config.Add("SignalisTalkSoundsSpeed", 0.075, "Speed of the signalis talk soun
 	category = "Signalis Talk Sounds"
 })
 
-local function PlaySSound(client, pitch)
-	client:EmitSound(sound, 60, pitch, 1, CHAN_AUTO)
-end
-
-local function OnTalk(client, chatType, message, anonymous)
-	if ix.config.Get("SignalisTalkSoundsEnabled", true) and chatType == "ic" then
-		-- split the message into a table of words, keeping in spaces
-		local words = string.Explode(" ", message)
-
-		local pitch, speed = client:GetTalkPitchAndSpeed()
-
-		client.sTalkInfo = {
-			words = words,
-			pitch = pitch,
-			speed = speed,
-			next = 0
-		}
+if SERVER then
+	local function PlaySSound(client, pitch)
+		client:EmitSound(sound, 60, pitch, 1, CHAN_AUTO)
 	end
-end
 
-local function OnTick()
-	if ix.config.Get("SignalisTalkSoundsEnabled", true) then
-		for _, client in ipairs(player.GetAll()) do
-			if client.sTalkInfo and client.sTalkInfo.next < CurTime() then
-				local speed = ix.config.Get("SignalisTalkSoundsSpeed", 0.075) * (2 - client.sTalkInfo.speed)
-				
-				client.sTalkInfo.next = CurTime() + speed
+	local function OnTalk(client, chatType, message, anonymous)
+		if ix.config.Get("SignalisTalkSoundsEnabled", true) and chatType == "ic" then
+			-- split the message into a table of words
+			local words = string.Explode(" ", message)
+			-- then split words that are longer than 6 chars into separate table entries by 5 chars
+			for i = 1, #words do
+				if #words[i] > 6 then
+					local newWords = {}
+					for j = 1, #words[i], 6 do
+						table.insert(newWords, string.sub(words[i], j, j + 5))
+					end
+					table.remove(words, i)
+					for j = 1, #newWords do
+						table.insert(words, i + j - 1, newWords[j])
+					end
+				end
+			end
 
-				-- grab the first word from the words
-				local firstChar = client.sTalkInfo.words[1]
+			local pitch, speed = client:GetTalkPitchAndSpeed()
 
-				PlaySSound(client, client.sTalkInfo.pitch)
+			client.sTalkInfo = {
+				words = words,
+				pitch = pitch,
+				speed = speed,
+				next = 0
+			}
+		end
+	end
 
-				-- remove the first word from the table
-				table.remove(client.sTalkInfo.words, 1)
+	local function OnTick()
+		if ix.config.Get("SignalisTalkSoundsEnabled", true) then
+			for _, client in ipairs(player.GetAll()) do
+				if client.sTalkInfo and client.sTalkInfo.next < CurTime() then
+					local speed = ix.config.Get("SignalisTalkSoundsSpeed", 0.075) * (2 - client.sTalkInfo.speed)
+					
+					client.sTalkInfo.next = CurTime() + speed
 
-				-- if the word table is empty, remove the table
-				if #client.sTalkInfo.words == 0 then
-					client.sTalkInfo = nil
+					-- grab the first word from the words
+					local firstChar = client.sTalkInfo.words[1]
+
+					PlaySSound(client, client.sTalkInfo.pitch)
+
+					-- remove the first word from the table
+					table.remove(client.sTalkInfo.words, 1)
+
+					-- if the word table is empty, remove the table
+					if #client.sTalkInfo.words == 0 then
+						client.sTalkInfo = nil
+					end
 				end
 			end
 		end
 	end
-end
 
-if ix.config.Get("SignalisTalkSoundsEnabled", true) then
-	hook.Add("PostPlayerSay", "SignalisTalkSounds_PostPlayerSay", OnTalk)
-	hook.Add("Tick", "SignalisTalkSounds_Tick", OnTick)
+	if ix.config.Get("SignalisTalkSoundsEnabled", true) then
+		hook.Add("PostPlayerSay", "SignalisTalkSounds_PostPlayerSay", OnTalk)
+		hook.Add("Tick", "SignalisTalkSounds_Tick", OnTick)
+	end
 end
-
