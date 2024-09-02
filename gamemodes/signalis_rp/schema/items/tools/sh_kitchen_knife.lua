2bd5252db2d474c5487192f81310f23d7cd4b435
Maja552 <mikazukikanade@gmail.com>
2024-09-02 17:59:55 +0200

    Made it so knife suicide drops the knife

--- gamemodes/signalis_rp/schema/items/tools/sh_kitchen_knife.lua
+++ gamemodes/signalis_rp/schema/items/tools/sh_kitchen_knife.lua
@@ -13,13 +13,23 @@ ITEM.functions.Suicide = {
     icon = "icon16/cross.png",
 	OnRun = function(item)
         if IsValid(item.player) then
+            local eyepos = item.player:EyePos()
+            local eyeang = item.player:EyeAngles()
+            local aimvector = item.player:GetAimVector()
+
+            item:Spawn(eyepos + (aimvector * 20), eyeang)
+
             local dmginfo = DamageInfo()
             dmginfo:SetDamage(item.player:Health())
             dmginfo:SetAttacker(item.player)
             dmginfo:SetDamageType(DMG_SLASH) 
         
             item.player:TakeDamageInfo(dmginfo)
+
+            return true
         end
+
+        return false
 	end,
 	OnCanRun = function(item)
 		return IsValid(item.player)
