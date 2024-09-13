2bdc5dc8786b240c4c9d449e52d2ab927ea98bb4
Maja552 <mikazukikanade@gmail.com>
2024-09-13 15:26:43 +0200

    Added small bottle

---
+++ gamemodes/signalis_rp/schema/items/misc/sh_small_bottle.lua
@@ -0,0 +1,5 @@
+
+ITEM.name = "Small Bottle"
+ITEM.model = Model("models/eternalis/items/misc/antique_bottle.mdl")
+ITEM.description = "A small bottle filled with a clear liquid. The contents smell sharply of ammonia. Probably some sort of smelling salts, an inhalant sometimes used to help restore consciousness and mental alertness."
+ITEM.skin = 0
