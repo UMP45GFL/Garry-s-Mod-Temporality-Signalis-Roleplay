1cb8bcee124fbfc9a4d70fb778b396def9734c39
Maja552 <mikazukikanade@gmail.com>
2024-09-13 15:21:26 +0200

    Made it so tray only accepts food items

--- gamemodes/signalis_rp/schema/items/containers/sh_tray.lua
+++ gamemodes/signalis_rp/schema/items/containers/sh_tray.lua
@@ -4,3 +4,7 @@ ITEM.model = Model("models/eternalis/items/rations/tray.mdl")
 ITEM.width = 2
 ITEM.height = 1
 ITEM.description = "A plastic tray with compartments for different types of food."
+
+ITEM.AcceptableBases = {
+    ["base_food"] = true
+}
