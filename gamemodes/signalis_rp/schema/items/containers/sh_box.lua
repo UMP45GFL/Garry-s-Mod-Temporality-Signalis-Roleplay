4c37f1faf0e83ff6c0438c1b16d3c035c8c097a6
Maja552 <mikazukikanade@gmail.com>
2024-09-13 15:24:45 +0200

    Added box

---
+++ gamemodes/signalis_rp/schema/items/containers/sh_box.lua
@@ -0,0 +1,9 @@
+ITEM.name = "Box"
+ITEM.description = "A small box."
+ITEM.model = Model("models/eternalis/items/library/paperbox.mdl")
+
+ITEM.invWidth = 3
+ITEM.invHeight = 2
+
+ITEM.width = 2
+ITEM.height = 2
