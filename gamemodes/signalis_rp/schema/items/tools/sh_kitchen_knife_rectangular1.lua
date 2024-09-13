475a5368287d69fcf523abd2230a879bee10bbd8
Maja552 <mikazukikanade@gmail.com>
2024-09-13 15:47:55 +0200

    Added rectangular kitchen knives

---
+++ gamemodes/signalis_rp/schema/items/tools/sh_kitchen_knife_rectangular1.lua
@@ -0,0 +1,16 @@
+
+ITEM.name = "Kitchen knife"
+ITEM.model = Model("models/eternalis/items/weapons/kitchen/longer_stronger_knife_01.mdl")
+ITEM.description = "A rectangular sharp kitchen knife. Perfect for cutting vegetables and meat."
+ITEM.skin = 0
+
+ITEM.width = 2
+ITEM.height = 1
+
+ITEM.canSlice = true
+
+ITEM.functions.Suicide = {
+    icon = "icon16/cross.png",
+	OnRun = SuicideItemFunction(item),
+	OnCanRun = CanSuicide(item)
+}
