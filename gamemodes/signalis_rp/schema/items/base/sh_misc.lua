e14bce1af3f4f3f4979494bfe3cebb0b38e29d37
Maja552 <mikazukikanade@gmail.com>
2024-09-13 16:28:42 +0200

    Changed misc category to misellaneous

--- gamemodes/signalis_rp/schema/items/base/sh_misc.lua
+++ gamemodes/signalis_rp/schema/items/base/sh_misc.lua
@@ -3,5 +3,5 @@ ITEM.name = "Misc Base"
 ITEM.model = "models/eternalis/items/misc/totem.mdl"
 ITEM.width = 1
 ITEM.height = 1
-ITEM.category = "Misc"
+ITEM.category = "Miscellaneous"
 ITEM.description = "Base misc item"
