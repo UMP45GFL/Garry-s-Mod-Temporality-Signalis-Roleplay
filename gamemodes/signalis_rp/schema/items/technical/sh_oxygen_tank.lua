9a455d80ba89ed6042831afa63d6b63d05052c83
Maja552 <mikazukikanade@gmail.com>
2024-09-14 23:47:35 +0200

    Added oxygen tank

---
+++ gamemodes/signalis_rp/schema/items/technical/sh_oxygen_tank.lua
@@ -0,0 +1,10 @@
+
+ITEM.name = "Oxygen tank"
+ITEM.model = Model("models/eternalis/items/technical/oxy_tank.mdl")
+ITEM.description = "A small oxygen tank."
+ITEM.skin = 0
+
+ITEM.width = 4
+ITEM.height = 1
+
+ITEM.weight = 3
