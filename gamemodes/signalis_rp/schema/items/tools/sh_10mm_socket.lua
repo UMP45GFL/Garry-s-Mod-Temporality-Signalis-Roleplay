977e8f4a01ed12351c71655094ad50ff5360fa0a
Maja552 <mikazukikanade@gmail.com>
2024-09-14 23:48:43 +0200

    Fixed socket models and added bodygroups to handle

--- gamemodes/signalis_rp/schema/items/tools/sh_10mm_socket.lua
+++ gamemodes/signalis_rp/schema/items/tools/sh_10mm_socket.lua
@@ -1,6 +1,6 @@
 
 ITEM.name = "10mm socket"
-ITEM.model = Model("models/eternalis/items/technical/wrenchsocket.mdl")
+ITEM.model = Model("models/eternalis/items/technical/wrenchsocket_10mm.mdl")
 ITEM.description = "A 10mm socket adapter for hexagonal bolts for use with a universal wrench. Without a handle, it's impossible to use."
 ITEM.skin = 0
 
--- gamemodes/signalis_rp/schema/items/tools/sh_wrench_socket_handle.lua
+++ gamemodes/signalis_rp/schema/items/tools/sh_wrench_socket_handle.lua
@@ -3,8 +3,16 @@ ITEM.name = "Socket wrench handle"
 ITEM.model = Model("models/eternalis/items/technical/wrenchsocket.mdl")
 ITEM.description = "A universal wrench handle with a snap-on pin for socket adapters. Useless without a socket adapter."
 ITEM.skin = 0
+ITEM.bodygroups = "01"
 
 ITEM.width = 1
 ITEM.height = 2
 
 ITEM.weight = 0.3
+
+ITEM.forceRender = true
+ITEM.iconCam = {
+	pos = Vector(-10, -5, -1),
+	ang = Angle(0, 30, 180),
+	fov = 55
+}
