4eac394689a7424e820bb65ca225eea28bf845b1
Maja552 <mikazukikanade@gmail.com>
2024-09-15 20:32:46 +0200

    Added kettle and mug

---
+++ gamemodes/signalis_rp/schema/items/misc/sh_kettle.lua
@@ -0,0 +1,7 @@
+
+ITEM.name = "Kettle"
+ITEM.model = Model("models/eternalis/items/rations/kettle.mdl")
+ITEM.description = "A kettle that can be used to boil water."
+ITEM.skin = 0
+
+ITEM.weight = 1
---
+++ gamemodes/signalis_rp/schema/items/misc/sh_mug.lua
@@ -0,0 +1,7 @@
+
+ITEM.name = "Mug"
+ITEM.model = Model("models/eternalis/items/rations/mug.mdl")
+ITEM.description = "A mug."
+ITEM.skin = 0
+
+ITEM.weight = 0.3
