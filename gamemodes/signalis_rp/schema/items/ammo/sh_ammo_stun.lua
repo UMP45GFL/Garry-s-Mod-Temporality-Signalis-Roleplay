7aecdd4c68e1d8683a5d5f8cf3f59bd91e49bcd1
Maja552 <mikazukikanade@gmail.com>
2024-09-17 22:50:14 +0200

    Fixerd stun baton ammo

--- gamemodes/signalis_rp/schema/items/ammo/sh_ammo_stun.lua
+++ gamemodes/signalis_rp/schema/items/ammo/sh_ammo_stun.lua
@@ -1,7 +1,7 @@
 ITEM.name = "Stun baton ammo"
 ITEM.model = "models/Items/battery.mdl"
 
-ITEM.ammo = "ammolaser" -- type of the ammo
+ITEM.ammo = "ammostun" -- type of the ammo
 ITEM.ammoAmount = 4 -- amount of the ammo
 ITEM.description = "Energy cell. Ammo for the stun baton."
 
