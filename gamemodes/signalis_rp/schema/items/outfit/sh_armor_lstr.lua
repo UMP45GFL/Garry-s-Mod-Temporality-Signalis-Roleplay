1c965d090d453883b1ffcac3502e49f4b3b41c72
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-11-26 01:16:50 -0800

    Fix capitalization in armor description

--- gamemodes/signalis_rp/schema/items/outfit/sh_armor_lstr.lua
+++ gamemodes/signalis_rp/schema/items/outfit/sh_armor_lstr.lua
@@ -1,6 +1,6 @@
 
 ITEM.name = "LSTR Armor"
-ITEM.description = "Bullet-Resistant Armor Plating."
+ITEM.description = "Bullet-resistant armor plating."
 ITEM.category = "Clothing"
 ITEM.model = "models/eternalis/items/equipment/lstr_armor.mdl"
 ITEM.outfitCategory = "body"
