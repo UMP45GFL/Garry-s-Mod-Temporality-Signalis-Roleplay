445ca0969f89ab500588427bf868d145bfc520ad
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-03 09:09:17 -0800

    food update 1 (pt 2)

---
+++ gamemodes/signalis_rp/schema/items/containers/sh_pizza_box.lua
@@ -0,0 +1,12 @@
+
+ITEM.name = "Pizza box"
+ITEM.model = Model("models/foodnhouseholditems/pizzabox.mdl")
+ITEM.description = "A cardboard pizza box."
+
+ITEM.invWidth = 2
+ITEM.invHeight = 2
+
+ITEM.width = 2
+ITEM.height = 2
+
+ITEM.weight = 1
