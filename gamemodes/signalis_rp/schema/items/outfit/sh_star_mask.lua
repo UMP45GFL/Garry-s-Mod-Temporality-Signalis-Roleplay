9f51078d31bc4e26b1ba5867f7088abf13494001
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-06 12:39:58 -0800

    Add STAR Mask item with equip logic

---
+++ gamemodes/signalis_rp/schema/items/outfit/sh_star_mask.lua
@@ -0,0 +1,30 @@
+
+ITEM.name = "STAR Mask"
+ITEM.description = "Face-covering mask for STAR units."
+ITEM.category = "Clothing"
+ITEM.model = "models/eternalis/items/equipment/pilotka_hat.mdl"
+ITEM.outfitCategory = "mask"
+
+ITEM.bodyGroups = {
+	["mask"] = 1
+}
+ITEM.bodyGroupsUnequipped = {
+	["mask"] = 0
+}
+
+ITEM.weight = 1
+
+ITEM.width = 1
+ITEM.height = 1
+
+function ITEM:CanEquipOutfit(client)
+    local character = client:GetCharacter()
+    if character and (
+        character.vars.class == "replika_star" or
+   ) then
+        return true
+    end
+
+    client:NotifyLocalized("cannotEquipOutfit")
+	return false
+end
