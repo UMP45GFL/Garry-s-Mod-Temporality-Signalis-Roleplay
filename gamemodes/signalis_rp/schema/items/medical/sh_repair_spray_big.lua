834633d46d4496ead28f0eaa0cb96bcde3fd639b
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-06 22:20:07 -0800

    Enhance Repair Spray item properties
    Updated the description and increased health restoration.

--- gamemodes/signalis_rp/schema/items/medical/sh_repair_spray_big.lua
+++ gamemodes/signalis_rp/schema/items/medical/sh_repair_spray_big.lua
@@ -1,11 +1,11 @@
 
 ITEM.name = "Repair spray+"
 ITEM.model = Model("models/eternalis/items/medical/health_spray_big.mdl")
-ITEM.description = "A single-use spray gun to fill damaged areas with fast-curing polyurethane-based foam."
+ITEM.description = "A single-use spray gun to fill damaged areas with fast-curing polyurethane-based foam. Restores a large amount of health."
 ITEM.price = 38
 
 ITEM.weight = 0.3
 
-ITEM.addHealth = 50
+ITEM.addHealth = 60
 ITEM.applySound = "eternalis/items/heal.wav"
 ITEM.replikaOnly = true
