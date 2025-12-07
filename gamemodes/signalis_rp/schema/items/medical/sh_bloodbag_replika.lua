7ceb2497d903f80d052a8aa26a05dc26bb38c493
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-06 22:18:56 -0800

    Add oxidant bag item to medical schema

---
+++ gamemodes/signalis_rp/schema/items/medical/sh_bloodbag_replika.lua
@@ -0,0 +1,12 @@
+
+ITEM.name = "Oxidant bag"
+ITEM.model = Model("models/eternalis/items/medical/bloodbag.mdl")
+ITEM.description = "A bag of oxidant."
+ITEM.category = "Medical"
+ITEM.price = 15
+ITEM.skin = 1
+
+ITEM.weight = 0.6
+
+ITEM.addHealth = 35
+ITEM.applySound = "items/medshot4.wav"
