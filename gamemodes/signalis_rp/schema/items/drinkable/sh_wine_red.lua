a31538baf82ec363ed6a58ee9646370d1a54e3d5
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-06 22:11:22 -0800

    Create sh_wine_red.lua

---
+++ gamemodes/signalis_rp/schema/items/drinkable/sh_wine_red.lua
@@ -0,0 +1,15 @@
+
+ITEM.name = "Red Wine"
+ITEM.model = Model("models/foodnhouseholditems/wine_red1.mdl")
+ITEM.description = "A bottle of red Wine. 15% alcohol by volume."
+ITEM.skin = 1
+ITEM.width = 1
+ITEM.height = 2
+
+ITEM.addHunger = 0
+ITEM.addThirst = -20
+ITEM.addHealth = 5
+ITEM.addStamina = 5
+ITEM.eatSound = "npc/barnacle/barnacle_gulp2.wav"
+
+ITEM.weight = 1
