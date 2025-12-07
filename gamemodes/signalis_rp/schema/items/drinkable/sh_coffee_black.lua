90472f59ba9ee378fa376fda0fc0bf69db804ebd
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-06 22:30:13 -0800

    Add black coffee item with properties and effects

---
+++ gamemodes/signalis_rp/schema/items/drinkable/sh_coffee_black.lua
@@ -0,0 +1,13 @@
+
+ITEM.name = "Mug of Coffee (Black)"
+ITEM.model = Model("models/eternalis/items/rations/mug.mdl")
+ITEM.description = "A mug of coffee, black."
+ITEM.skin = 0
+
+ITEM.addHunger = 0
+ITEM.addThirst = -10
+ITEM.addHealth = 5
+ITEM.addStamina = 40
+ITEM.eatSound = "npc/barnacle/barnacle_gulp2.wav"
+
+ITEM.weight = 0.3
