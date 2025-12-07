52cb53047a76c92162fd21c01a9933db5b59c83e
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-06 22:23:32 -0800

    Rename Blood bag to IV bag and update properties

---
+++ gamemodes/signalis_rp/schema/items/medical/sh_ivbag.lua
@@ -0,0 +1,12 @@
+
+ITEM.name = "IV bag"
+ITEM.model = Model("models/eternalis/items/medical/iv_pack.mdl")
+ITEM.description = "A bag of intravenuous fluid."
+ITEM.category = "Medical"
+ITEM.price = 15
+ITEM.bodygroups = "01"
+
+ITEM.weight = 0.4
+
+ITEM.addHealth = 15
+ITEM.applySound = "items/medshot4.wav"
