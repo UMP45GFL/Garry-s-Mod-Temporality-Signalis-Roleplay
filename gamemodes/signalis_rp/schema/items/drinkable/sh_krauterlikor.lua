8c13f32f90c62e072c9f661d5ce11562c80f0918
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-06 22:09:30 -0800

    Update Kräuterlikör model and add skin property

---
+++ gamemodes/signalis_rp/schema/items/drinkable/sh_krauterlikor.lua
@@ -0,0 +1,15 @@
+
+ITEM.name = "Kräuterlikör"
+ITEM.model = Model("models/props_se/junk/bottle_4.mdl")
+ITEM.description = "A bottle of Kräuterlikör. 35% alcohol by volume."
+ITEM.skin = 1
+ITEM.width = 1
+ITEM.height = 2
+
+ITEM.addHunger = 0
+ITEM.addThirst = -10
+ITEM.addHealth = 5
+ITEM.addStamina = 5
+ITEM.eatSound = "npc/barnacle/barnacle_gulp2.wav"
+
+ITEM.weight = 1
