252ac5dff2c5ab8445e71b6b56f8de3581106964
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-06 22:12:08 -0800

    Fix white wine description and stamina value
    Updated the description and adjusted stamina value for white wine.

---
+++ gamemodes/signalis_rp/schema/items/drinkable/sh_wine_white.lua
@@ -0,0 +1,14 @@
+
+ITEM.name = "White Wine"
+ITEM.model = Model("models/foodnhouseholditems/wine_white3.mdl")
+ITEM.description = "A bottle of red Wine. 6% alcohol by volume."
+ITEM.width = 1
+ITEM.height = 2
+
+ITEM.addHunger = 0
+ITEM.addThirst = -20
+ITEM.addHealth = 5
+ITEM.addStamina = 15
+ITEM.eatSound = "npc/barnacle/barnacle_gulp2.wav"
+
+ITEM.weight = 1
