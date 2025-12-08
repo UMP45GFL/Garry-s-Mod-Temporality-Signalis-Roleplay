c8bd894d1baea37e334ca4d206aac27511cb9dad
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-08 14:05:29 -0800

    Add cake recipe me lazy

---
+++ gamemodes/signalis_rp/plugins/ixcraft/recipes/sh_recipe_cake.lua
@@ -0,0 +1,14 @@
+
+RECIPE.name = "Cake"
+RECIPE.description = "Craft a cake."
+RECIPE.model = "models/foodnhouseholditems/sandwich.mdl"
+RECIPE.requirements = {
+	["pastry_mix"] = 1,
+	["sugar"] = 1,
+}
+RECIPE.results = {
+	["cake"] = 1,
+}
+RECIPE.tools = {
+	"kitchen_knife"
+}
