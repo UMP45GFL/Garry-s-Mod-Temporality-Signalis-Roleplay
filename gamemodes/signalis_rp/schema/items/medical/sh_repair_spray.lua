f3c19d6fc90a48f3084bfded453492821df2ca14
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-09 13:21:41 -0800

    add medical category to repair spray

--- gamemodes/signalis_rp/schema/items/medical/sh_repair_spray.lua
+++ gamemodes/signalis_rp/schema/items/medical/sh_repair_spray.lua
@@ -2,6 +2,7 @@
 ITEM.name = "Repair spray"
 ITEM.model = Model("models/eternalis/items/medical/health_spray_small.mdl")
 ITEM.description = "A single-use spray gun to fill damaged areas with polyurethane-based expanding foam. Restores a decent amount of health."
+ITEM.category = "Medical"
 ITEM.price = 18
 
 ITEM.weight = 0.3
