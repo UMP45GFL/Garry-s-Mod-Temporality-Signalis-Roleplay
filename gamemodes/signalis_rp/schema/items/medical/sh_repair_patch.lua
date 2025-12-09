6286c426b4984780addbd6a78cfa3601a01853fb
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-09 13:21:45 -0800

    add medical category to repair patch

--- gamemodes/signalis_rp/schema/items/medical/sh_repair_patch.lua
+++ gamemodes/signalis_rp/schema/items/medical/sh_repair_patch.lua
@@ -2,6 +2,7 @@
 ITEM.name = "Repair patch"
 ITEM.model = Model("models/eternalis/items/medical/health_patch.mdl")
 ITEM.description = "A sealed, single-use adhesive patch containing a coagulation agent to patch up damaged areas. Restores a small amount of health over time."
+ITEM.category = "Medical"
 ITEM.price = 18
 
 ITEM.weight = 0.1
