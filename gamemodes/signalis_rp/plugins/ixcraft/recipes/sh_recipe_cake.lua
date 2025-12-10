7a39dbfb19ac682cbdd91f72225ebad55e50f0e6
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-09 19:08:43 -0800

    cake recipe model

--- gamemodes/signalis_rp/plugins/ixcraft/recipes/sh_recipe_cake.lua
+++ gamemodes/signalis_rp/plugins/ixcraft/recipes/sh_recipe_cake.lua
@@ -1,10 +1,9 @@
 
 RECIPE.name = "Cake"
-RECIPE.description = "Craft a cake."
-RECIPE.model = "models/foodnhouseholditems/sandwich.mdl"
+RECIPE.description = "Craft a cake using pastry mix."
+RECIPE.model = "models/foodnhouseholditems/cake_b.mdl"
 RECIPE.requirements = {
 	["pastry_mix"] = 1,
-	["sugar"] = 1,
 }
 RECIPE.results = {
 	["cake"] = 1,
