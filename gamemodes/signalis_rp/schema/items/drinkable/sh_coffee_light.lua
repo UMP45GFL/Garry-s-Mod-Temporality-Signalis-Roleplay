fad6296c89991d5bb21e2721809f23e6ebcf8785
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2025-12-25 00:17:47 -0800

    Add sh_coffee_light.lua for light coffee item

---
+++ gamemodes/signalis_rp/schema/items/drinkable/sh_coffee_light.lua
@@ -0,0 +1,13 @@
+
+ITEM.name = "Mug of Coffee (Light)"
+ITEM.model = Model("models/eternalis/items/rations/mug.mdl")
+ITEM.description = "A mug of coffee, light."
+ITEM.skin = 1
+
+ITEM.addHunger = 0
+ITEM.addThirst = -10
+ITEM.addHealth = 5
+ITEM.addStamina = 40
+ITEM.eatSound = "npc/barnacle/barnacle_gulp2.wav"
+
+ITEM.weight = 0.3
