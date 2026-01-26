e9783c21d62b69325b9b563a768e0dbef3c6fb5c
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2026-01-26 01:44:58 -0800

    toolbox to blue

--- gamemodes/signalis_rp/schema/items/containers/sh_tool_box.lua
+++ gamemodes/signalis_rp/schema/items/containers/sh_tool_box.lua
@@ -1,6 +1,7 @@
-ITEM.name = "Tool box"
-ITEM.description = "A small tool box."
+ITEM.name = "Tool box (Blue)"
+ITEM.description = "A small blue tool box."
 ITEM.model = Model("models/eternalis/items/equipment/toolbox.mdl")
+ITEM.skin = 0
 
 ITEM.invWidth = 4
 ITEM.invHeight = 2
