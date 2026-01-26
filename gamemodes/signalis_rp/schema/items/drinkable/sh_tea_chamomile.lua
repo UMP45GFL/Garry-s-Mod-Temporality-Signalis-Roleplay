4a166537f78fb589e91881031be8d7aa11dfb207
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2026-01-26 02:07:04 -0800

    chamomile tea

---
+++ gamemodes/signalis_rp/schema/items/drinkable/sh_tea_chamomile.lua
@@ -0,0 +1,12 @@
+ITEM.name = "Cup of Kamille Tea"
+ITEM.model = Model("models/eternalis/items/rations/cupfilled.mdl")
+ITEM.description = "A cup of kamille tea."
+ITEM.skin = 5
+
+ITEM.addHunger = 0
+ITEM.addThirst = -10
+ITEM.addHealth = 5
+ITEM.addStamina = 40
+ITEM.eatSound = "npc/barnacle/barnacle_gulp2.wav"
+
+ITEM.weight = 0.3
