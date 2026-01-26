eef27cd84487864daf9554b46b3206a81e34ad11
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2026-01-26 01:45:41 -0800

    green tool box

---
+++ gamemodes/signalis_rp/schema/items/containers/sh_tool_box_green.lua
@@ -0,0 +1,12 @@
+ITEM.name = "Tool box (Green)"
+ITEM.description = "A small green tool box."
+ITEM.model = Model("models/eternalis/items/equipment/toolbox.mdl")
+ITEM.skin = 2
+
+ITEM.invWidth = 4
+ITEM.invHeight = 2
+
+ITEM.width = 2
+ITEM.height = 2
+
+ITEM.weight = 2
