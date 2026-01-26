422b4baab26873d2ad7911e174094cb62b11bbe3
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2026-01-26 01:50:07 -0800

    Add Cup of Green Tea item with properties

---
+++ gamemodes/signalis_rp/schema/items/drinkable/sh_tea_green.lua
@@ -0,0 +1,13 @@
+
+ITEM.name = "Cup of Green Tea"
+ITEM.model = Model("models/eternalis/items/rations/cupfilled.mdl")
+ITEM.description = "A cup of green tea."
+ITEM.skin = 7
+
+ITEM.addHunger = 0
+ITEM.addThirst = -10
+ITEM.addHealth = 5
+ITEM.addStamina = 40
+ITEM.eatSound = "npc/barnacle/barnacle_gulp2.wav"
+
+ITEM.weight = 0.3
