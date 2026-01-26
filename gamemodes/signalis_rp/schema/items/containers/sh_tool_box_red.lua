2ef1b59ef3d8e6e442c2a45465a2607de53a0670
UMP45 <138188821+UMP45GFL@users.noreply.github.com>
2026-01-26 01:45:59 -0800

    red rool box

---
+++ gamemodes/signalis_rp/schema/items/containers/sh_tool_box_red.lua
@@ -0,0 +1,12 @@
+ITEM.name = "Tool box (Red)"
+ITEM.description = "A small red tool box."
+ITEM.model = Model("models/eternalis/items/equipment/toolbox.mdl")
+ITEM.skin = 1
+
+ITEM.invWidth = 4
+ITEM.invHeight = 2
+
+ITEM.width = 2
+ITEM.height = 2
+
+ITEM.weight = 2
